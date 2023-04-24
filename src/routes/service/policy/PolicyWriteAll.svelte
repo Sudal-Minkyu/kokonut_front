a3e0f3215ba8e9a468bdc008658ebbc795e09a30
우디 <woody@kokonut.me>
2023-04-24 11:09:06 +0900

    개인정보처리방침 페이지 추가 작업중

---
+++ src/routes/service/policy/PolicyWriteAll.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from "../../../components/service/layout/Header.svelte"
+
+    import { page } from '../../../lib/store.js'
+
+    import PolicyWriteStep1 from '../../../components/service/policy/PolicyWriteStep1.svelte'
+
+</script>
+
+<Header />
+<section class="stepbodyWrap">
+    <div class="stepcontentInnerWrap">
+
+        <PolicyWriteStep1 />
+
+
+    </div>
+</section>
