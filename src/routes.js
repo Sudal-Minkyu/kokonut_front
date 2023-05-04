823323d4f8e3119c3ae2370e6cf140772667c2be
서트 <cert@kokonut.me>
2023-05-04 12:38:57 +0000

    Edited routes.js

--- src/routes.js
+++ src/routes.js
@@ -110,8 +110,8 @@ export default {
     // 1:1문의 상세보기페이지
     "/service/policyDetail/:piId": PolicyDetail,
     // 개인정보처리방침 모든작성페이지
-    "/service/PolicyWriteAll": wrap({
-        asyncComponent: () => import("./routes/service/PolicyWriteAll.svelte"),
+    "/service/policy//PolicyWriteAll": wrap({
+        asyncComponent: () => import("./routes/service/policy//PolicyWriteAll.svelte"),
     }),
 
     // 개인정보제공 리스트 페이지
