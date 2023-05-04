b999999401ee3f56ddf1b23c82ca007630045fb9
서트 <cert@kokonut.me>
2023-05-04 09:11:16 +0000

    Edited install_dependencies.sh

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -6,10 +6,8 @@ sudo yum update -y
 
 # install node.js
 #curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo bash -
-sudo yum install -y nodejs
+#sudo yum install -y nodejs
 
 # install npm packages
 cd /root/kokonut_frontend/
-#npm install --global npm
-npm install --save babel-core
+npm install --global npm
