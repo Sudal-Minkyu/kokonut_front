e552583d38109fc8630309bc2830e34209d34877
서트 <cert@kokonut.me>
2023-05-05 05:17:07 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Stop existing process
+echo "Stopping existing process..."
+PID=$(ps -ef | grep node | grep -v grep | awk '{print $2}')
+if [ -n "$PID" ]; then
+    echo "Killing process $PID"
+    kill -9 $PID
+fi
+
 # Change directory to the location of the JAR file
 cd /root/kokonut_frontend/
-
-# 프로세스를 죽입니다.
-pkill -f node
 
 # 10초간 대기합니다.
 sleep 10
@@ -12,3 +17,4 @@ sleep 10
 # 새로운 프로세스를 시작합니다.
 #npm run dev
 nohup npm run dev &
+
