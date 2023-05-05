9f8e7481a4aba6c7f606bb2ba094ffa47a38741b
서트 <cert@kokonut.me>
2023-05-05 05:07:47 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -3,6 +3,12 @@
 # Change directory to the location of the JAR file
 cd /root/kokonut_frontend/
 
-# Start the application
+# 프로세스를 죽입니다.
+pkill -f node
+
+# 10초간 대기합니다.
+sleep 10
+
+# 새로운 프로세스를 시작합니다.
 #npm run dev
 nohup npm run dev &
