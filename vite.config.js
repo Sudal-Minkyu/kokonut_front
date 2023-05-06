5a6ec4e21c3f2b14b30b2d4a414514889e8cb09e
서트 <cert@kokonut.me>
2023-05-06 13:37:53 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+
 import fs from 'fs';
 import https from 'https';
-import { getCertificate, getPrivateKey } from '@aws-sdk/client-acm';
+// import { getCertificate, getPrivateKey } from '@aws-sdk/client-acm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,18 +12,21 @@ export default defineConfig({
     https: async () => {
       const REGION = 'ap-northeast-2'; // AWS Region
       const CERTIFICATE_ARN = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423'; // SSL/TLS 인증서 ARN
+      
+      const acm = require('@aws-sdk/client-acm');
+      const { getCertificate, getPrivateKey } = acm;
 
       const getCredentials = () => {
         // AWS SDK credentials 설정
       };
-
+      
       const getCertificateAndKey = async () => {
-        const acm = new ACMClient({ region: REGION, credentials: await getCredentials() });
-        const data = await acm.exportCertificate({ CertificateArn: CERTIFICATE_ARN });
-        return {
-          certificate: data.Certificate,
-          privateKey: data.PrivateKey
-        };
+          const acm = new acm.ACMClient({ region: REGION, credentials: await getCredentials() });
+          const data = await acm.exportCertificate({ CertificateArn: CERTIFICATE_ARN });
+          return {
+              certificate: data.Certificate,
+              privateKey: data.PrivateKey
+          };
       };
 
       const { certificate, privateKey } = await getCertificateAndKey();
