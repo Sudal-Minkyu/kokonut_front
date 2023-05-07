5af2253d12f519d254e5bbeed89462c8e6aff3b1
서트 <cert@kokonut.me>
2023-05-07 04:19:25 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,22 @@
 
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { getCertificate, getPrivateKey } from '@aws-sdk/client-acm'
-import { fromEnv } from '@aws-sdk/credential-provider-env'
- 
+import { default as ACM } from '@aws-sdk/client-acm';
+
+
  // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [svelte()],
    
-   server: {
-   https: async () => {
-      const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';     // 인증서 ARN 값
-      const { Certificate, PrivateKey } = await getCertificate({ CertificateArn: certificateArn, credentials: await getCredentials() });
+  server: {
+    https: async () => {
+      const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423'; // 여기에 인증서 ARN 값을 넣으세요.
+      const acm = new ACM({});
+      const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
       return {
-        key: getPrivateKey(PrivateKey),
+        key: PrivateKey,
         cert: Certificate
       };
     }
   }
 });
- 
-const getCredentials = () => {
-  return fromEnv({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
-  });
-};
