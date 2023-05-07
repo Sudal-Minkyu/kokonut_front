5aa7e8ab8ffff7a35eb8aa079a8223c40b67ba3b
서트 <cert@kokonut.me>
2023-05-07 04:51:26 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import node from '@sveltejs/adapter-node';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { default as ACM } from '@aws-sdk/client-acm';
+import node from '@sveltejs/adapter-node';
 
 export default defineConfig({
   plugins: [svelte()],
@@ -10,24 +10,23 @@ export default defineConfig({
     outDir: 'build',
     assetsDir: 'static',
     minify: true,
-    sourcemap: false
+    sourcemap: false,
   },
   kit: {
     adapter: node(),
     target: '#svelte',
     vite: {
       server: {
-        https: true,
         https: async () => {
           const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';
           const acm = new ACM({});
           const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
           return {
             key: PrivateKey,
-            cert: Certificate
+            cert: Certificate,
           };
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
