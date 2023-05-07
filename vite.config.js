470a8342f90e1559cf61e50a4bb9b36d02bc62a8
서트 <cert@kokonut.me>
2023-05-07 04:42:27 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
-
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import node from '@sveltejs/adapter-node';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { default as ACM } from '@aws-sdk/client-acm';
 
-
- // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-   
-  server: {
-    https: async () => {
-      const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423'; // 여기에 인증서 ARN 값을 넣으세요.
-      const acm = new ACM({});
-      const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
-      return {
-        key: PrivateKey,
-        cert: Certificate
-      };
-    }
-  }
-  
   build: {
-    outDir: 'dist' // 빌드된 파일의 경로 설정
+    target: 'es2018',
+    outDir: 'build',
+    assetsDir: 'static',
+    minify: true,
+    sourcemap: false
+  },
+  kit: {
+    adapter: node(),
+    target: '#svelte',
+    vite: {
+      server: {
+        https: true,
+        https: async () => {
+          const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';
+          const acm = new ACM({});
+          const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
+          return {
+            key: PrivateKey,
+            cert: Certificate
+          };
+        }
+      }
+    }
   }
 });
