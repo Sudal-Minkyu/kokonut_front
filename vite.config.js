7b3f59cbd02248477a977d64ba3c02ea16b09e2a
서트 <cert@kokonut.me>
2023-05-10 15:48:39 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { default as ACM } from '@aws-sdk/client-acm';
@@ -6,55 +5,52 @@ import node from '@sveltejs/adapter-node';
 import axios from 'axios';
 
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
 
-  base: '/',
-  build: {
-    target: 'es2018',
-    outDir: 'build',
-    assetsDir: 'static',
-    minify: true,
-    sourcemap: false,
-  },
+    base: '/',
+    build: {
+        target: 'es2018',
+        outDir: 'build',
+        assetsDir: 'static',
+        minify: true,
+        sourcemap: false,
+        },
 
-  kit: {
-    adapter: node(),
-    target: '#svelte',
-    vite: {
-      server: {
-        host: '0.0.0.0',
-        port: 5173,
-        middlewareMode: true,
-        https: async () => {
-          const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';
-          const acm = new ACM({});
-          const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
-          const credentials = { key: PrivateKey, cert: Certificate };
-          return credentials;
+    kit: {
+        adapter: node(),
+        target: '#svelte',
+        vite: {
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            middlewareMode: true,
+            https: async () => {
+        const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';
+        const acm = new ACM({});
+        const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
+        const credentials = { key: PrivateKey, cert: Certificate };
+        return credentials;
         },
-        proxy: {
-          '*': {
-            target: 'https://beta.kokonut.me:8050',
-            changeOrigin: true,
-          },
+        
+    proxy: {
+    '': {
+        target: 'https://beta.kokonut.me:8050',
+        changeOrigin: true,
         },
-      },
     },
-  },
+    },
 
-middleware: async (app, { server }) => {
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  }); // Add closing parenthesis here
+    middlewares: async (app, { server }) => {   
+        app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
 
-  const instance = axios.create({
-    baseURL: 'https://beta.kokonut.me:8050',
-  });
-
-}
+    const instance = axios.create({
+      baseURL: 'https://beta.kokonut.me:8050',
+    });
 
     function saveToken(token) {
       localStorage.setItem('access_token', token);
@@ -99,6 +95,8 @@ middleware: async (app, { server }) => {
     } catch (error) {
       console.log(error);
     }
-})
+  },
+},
 
-
+},
+});
