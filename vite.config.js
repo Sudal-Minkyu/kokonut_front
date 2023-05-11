7a8825d065a6a54aeea30ca590b3aa4bf0d0c3ee
서트 <cert@kokonut.me>
2023-05-11 00:00:41 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -33,8 +33,8 @@ export default defineConfig({
         },
         
     proxy: {
-    '': {
-        target: 'http://localhost:8050',
+    '/*': {
+        target: 'https://beta.kokonut.me:8050',
         changeOrigin: true,
         },
     },
@@ -49,7 +49,7 @@ export default defineConfig({
     });
 
     const instance = axios.create({
-      baseURL: 'http://localhost:8050',
+      baseURL: '/*',
     });
 
     function saveToken(token) {
