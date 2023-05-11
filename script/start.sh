9b2d00e91f171f991a5c8a6a6a5608519d4b35c9
서트 <cert@kokonut.me>
2023-05-11 10:00:16 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,14 +2,11 @@
 
 cd /root/kokonut_frontend/
 
-# 프로세스 종료
 PID=$(pgrep -f npm)
 kill $PID
 
-# 10초간 대기합니다.
-sleep 10
+sleep 5
 
-# 새로운 프로세스를 시작합니다.
 nohup npm run dev -- --host 0.0.0.0 1>node.stdout 2>node.stderr &
 
 exit
