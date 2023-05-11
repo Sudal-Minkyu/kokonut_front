4f05dc87adb1b7f3050736a75d879d52d2d7c19f
서트 <cert@kokonut.me>
2023-05-11 07:35:04 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,41 +3,40 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { default as ACM } from '@aws-sdk/client-acm';
 
 export default defineConfig({
-    plugins: [svelte()],
+  plugins: [svelte()],
 
-    base: '/',
-    build: {
-        target: 'es2018',
-        outDir: 'build',
-        assetsDir: 'static',
-        minify: true,
-        sourcemap: false,
-        }, 
+  base: '/',
+  build: {
+    target: 'es2018',
+    outDir: 'build',
+    assetsDir: 'static',
+    minify: true,
+    sourcemap: false,
+  },
 
-    kit: {
-  //    adapter: node(),
-        target: '#svelte',
-        vite: {
-            server: {
-                host: '0.0.0.0',
-                port: 5173,
-                middlewareMode: true,
-                https: async () => {
-                    const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';
-                    const acm = new ACM({});
-                    const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
-                    const credentials = { key: PrivateKey, cert: Certificate };
-                    return credentials;
-                    },
-        
-                proxy: {
-                    '/*': {
-                        target: 'http://localhost:8050',
-                        changeOrigin: true,
-                        },
-                    },
-            },
+  kit: {
+    target: '#svelte',
+    vite: {
+      server: {
+        host: '0.0.0.0',
+        port: 5173,
+        middlewareMode: true,
+        https: async () => {
+          const certificateArn = 'arn:aws:acm:ap-northeast-2:352166812188:certificate/95fe692e-f69e-42d0-9f48-e7dc29375423';
+          const acm = new ACM({});
+          const { Certificate, PrivateKey } = await acm.getCertificate({ CertificateArn: certificateArn });
+          const credentials = { key: PrivateKey, cert: Certificate };
+          return credentials;
         },
-    }, //kit
-    
+
+        proxy: {
+          '/*': {
+            target: 'https://localhost:8050',
+            changeOrigin: true,
+            secure: true,
+          },
+        },
+      },
+    },
+  },
 });
