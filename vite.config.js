c0de03195efde603164e42174dab3e69a1f6b584
서트 <cert@kokonut.me>
2023-05-12 07:40:23 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { createProxy } from 'vite-plugin-proxy';
+import proxy from 'vite-plugin-proxy';
 
-const proxy = require('vite-plugin-proxy');
-
-export default {
-  plugins: [proxy.createProxy({
-    '/v*/api': {
-      target: 'http://localhost:8050',
-      changeOrigin: true,
-      secure: false,
-      rewrite: (path) => path.replace(/^\/v*\/api/, '')
-    }
-  })]
-};
+export default defineConfig({
+  plugins: [
+    svelte(),
+    proxy.default({
+      '/v*/api': {
+        target: 'http://localhost:8050',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/v*\/api/, '')
+      }
+    })
+  ]
+});
