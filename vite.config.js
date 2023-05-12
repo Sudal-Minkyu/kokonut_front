ed6046e7eebf879f514c558a9a6bc8e50bdd5a45
서트 <cert@kokonut.me>
2023-05-12 08:13:36 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-const proxy = require('vite-plugin-proxy');
+import vitePluginProxy from 'vite-plugin-proxy'
 
 export default defineConfig({
-  plugins: [proxy.createProxy({
+  plugins: [vitePluginProxy({
     '/v*/api': {
       target: 'http://localhost:8050',
       changeOrigin: true,
