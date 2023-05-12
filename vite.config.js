5ae2330477513113e61a038e2479d66b9f31db8f
서트 <cert@kokonut.me>
2023-05-12 11:41:09 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-
 export default defineConfig({
   plugins: [svelte()],
-    server: {
+  server: {
     proxy: {
       '/v*/api': {
         target: 'http://localhost:8050',
         changeOrigin: true,
         secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
+        },
         rewrite: (path) => path.replace(/^\/v+\//, '/')
       }
     }
   }
 })
-
