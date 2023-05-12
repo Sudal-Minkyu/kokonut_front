1c7f85ec73a7322cbe5532e61f7eaa60cf76ff36
서트 <cert@kokonut.me>
2023-05-12 12:09:12 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/v*/api': {
-        target: 'http://localhost:8050',
+        target: 'https://beta.kokonut.me:8050',
         changeOrigin: true,
         secure: false,
         headers: {
