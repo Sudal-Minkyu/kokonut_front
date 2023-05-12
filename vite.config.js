6b2d8c250c7b04a3910e3db195b98aa627e61756
서트 <cert@kokonut.me>
2023-05-12 00:23:40 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
         },
         proxy: {
 //      '/^.*api\/.+': {
-        '*': {
+        '/**': {
             target: 'https://beta.kokonut.me:8050',
             changeOrigin: true,
 //          logLevel : 'debug',
