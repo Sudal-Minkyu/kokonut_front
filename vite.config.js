1bcf0a02661b4bb5643a1e6e7d23745e3a0b649c
서트 <cert@kokonut.me>
2023-05-13 11:14:02 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import axios from 'axios';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
 
-})
+  // axios 설정 추가
+  server: {
+    proxy: {
+      '*': {
+        target: 'https://beta-api.kokonut.me:8050', // API 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/*/, ''),
+      },
+    },
+  },
+});
+
+// axios를 사용하여 API 요청 보내는 함수 작성
+
+export async function request(url, method, data) {
+  try {
+    const response = await axios({
+      method: method,
+      url: `*`,
+      data: data,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('API 요청 에러');
+  }
+}
