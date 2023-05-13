819a36c3edcc2c7a711e95a3b02de44186443f73
서트 <cert@kokonut.me>
2023-05-13 06:28:40 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ export default defineConfig({
   plugins: [svelte()],
   server: {
     proxy: {
-      '*': {
-        target: 'https://beta.kokonut.me:8050',
-        changeOrigin: true,
-        headers: {
-          'Origin': 'https://beta.kokonut.me:5173',
-        },
-        rewrite: (path) => path.replace(/^\/v+\//, '/')
+      target: 'https://beta.kokonut.me:8050',
+      changeOrigin: true,
+      headers: {
+        'Origin': 'https://beta.kokonut.me:5173',
+      },
+      rewrite: (path) => path.replace(/^\/v+\//, '/'),
+      onProxyRes: (proxyRes) => {
+        proxyRes.headers['Access-Control-Allow-Origin'] = 'https://beta.kokonut.me:5173';
       }
     }
   }
