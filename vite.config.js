a3c2723b0b4c0a6b8a9466bce202547d4bca402a
서트 <cert@kokonut.me>
2023-05-13 02:31:15 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,7 @@ export default defineConfig({
       '/v*/api/**': {
         target: 'http://beta.kokonut.me:8050',
         changeOrigin: true,
-        secure: true,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST',
-          'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin',
           'Origin': 'https://beta.kokonut.me:5173',
         },
         rewrite: (path) => path.replace(/^\/v+\//, '/')
