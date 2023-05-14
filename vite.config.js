fec529889c9e4d37870c4fa5605feed9c70bf5fc
서트 <cert@kokonut.me>
2023-05-14 11:41:08 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/v*/api/**': {
-        target: 'http://172.16.16.173:8050',
+        target: 'https://beta-api.kokonut.me:8050',
         changeOrigin: true,
       },
     },
