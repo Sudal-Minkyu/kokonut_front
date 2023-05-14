dc5f81ba4b88750067293fa244209eaf1e4714b8
서트 <cert@kokonut.me>
2023-05-14 02:55:20 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,30 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'https://beta-api.kokonut.me:8050',
+  timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// JWT 토큰을 요청 헤더에 추가하는 Interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default defineConfig({
   plugins: [svelte()]
-
 });
+
+export { instance };
