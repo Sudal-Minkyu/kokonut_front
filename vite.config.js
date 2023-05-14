d1a2025d8b2f40577fa33f7e65bffd988c9bcd21
서트 <cert@kokonut.me>
2023-05-14 05:59:11 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+
+const env = {
+  VITE_SERVER_URL: process.env.VITE_SERVER_URL,
+};
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    proxy: {
+      '/v*/api/**': {
+        target: 'https://beta-api.kokonut.me:8050',
+        changeOrigin: true,
+      },
+    },
+  },
+  define: {
+    'process.env': {
+      VITE_SERVER_URL: JSON.stringify(env.VITE_SERVER_URL),
+    },
+  },
 });
