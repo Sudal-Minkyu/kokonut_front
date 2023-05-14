0d4d2bd3a00246441fe0e67aebba4b886fc42dca
서트 <cert@kokonut.me>
2023-05-14 05:28:39 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
-// import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig, loadEnv } from 'vite'
 
-// export default defineConfig({
-//   plugins: [svelte()]
-// });
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [svelte()],
-    define: {
-      __APP_TITLE__: env.APP_TITLE,
-      __API_URL__: env.API_URL
-    },
-  };
+export default defineConfig({
+  plugins: [svelte()]
 });
