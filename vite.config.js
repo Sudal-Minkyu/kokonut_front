2621a05756629b493c7c1aac61e060f5566954ee
서트 <cert@kokonut.me>
2023-05-15 04:15:53 +0000

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: "https://betaapi.kokonut.me",
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
 
 export default defineConfig({
-  plugins: [svelte()],
-  axios: instance
+  plugins: [svelte()]
 });
