95eb065537c642af54ef799533c6f4063e7f05e8
우디 <woody@kokonut.me>
2023-07-13 20:19:22 +0900

    기본로컬주소 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
- import { defineConfig } from 'vite'
- import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
  
- // https://vitejs.dev/config/
- export default defineConfig({
-   plugins: [svelte()],
- })
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [svelte()],
+
+    server: {
+        port: 5173,
+        host: '127.0.0.1'
+    }
+
+})
