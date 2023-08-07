2740638cd21000bb69d82b6064c7b3fb44d5e98b
우디 <woody@kokonut.me>
2023-08-07 11:52:29 +0900

    관리자활동이력 테이블

--- src/components/service/admin/activity/ActivityTable.svelte
+++ src/components/service/admin/activity/ActivityTable.svelte
@@ -12,16 +12,25 @@
 
     <table>
         <caption>관리자 활동 이력 리스트</caption>
+        <colgroup>
+            <col style="width:5.48%;">
+            <col style="width:13.59%;">
+            <col style="width:10.96%;">
+            <col style="width:15.4%;">
+            <col style="width:20.07%;">
+            <col style="width:25.07%;">
+            <col style="width:11.44%;">
+        </colgroup>
         <thead>
-        <tr>
-            <th>No</th>
-            <th>관리자</th>
-            <th>등급</th>
-            <th>활동내역</th>
-            <th>활동상세내역</th>
-            <th>활동일시(접속IP)</th>
-            <th>상태</th>
-        </tr>
+            <tr>
+                <th>No</th>
+                <th>관리자</th>
+                <th>등급</th>
+                <th>활동내역</th>
+                <th>활동상세내역</th>
+                <th>활동일시(접속IP)</th>
+                <th>상태</th>
+            </tr>
         </thead>
         <tbody>
         {#if activity_list.length !== 0}
@@ -36,7 +45,7 @@
                     {/if}
                     <td>{activity.activityCode}</td>
                     <td>
-                        <div class="reason">{activity.ahActivityDetail}</div>
+                        <div>{activity.ahActivityDetail}</div>
                     </td>
                     <td>{activity.insert_date}(IP:{activity.ahIpAddr})</td>
                     <td>
