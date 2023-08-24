5b021c28c0ac829770041973100dc43eb33be9e1
우디 <woody@kokonut.me>
2023-08-24 13:36:56 +0900

    메인 빌드테스트6

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # update packages
-#sudo yum update -y
+sudo yum update -y
 
 # install node.js
 #curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 sudo su -
+
+yum update -y
+
+# Change directory to the location of package.json file
 cd /root/kokonut_frontend/
+
+# Remove node_modules folder
+rm -rf node_modules
+
+# Install dependencies
+npm install
 
 PID=$(pgrep -f npm)
 kill $PID
