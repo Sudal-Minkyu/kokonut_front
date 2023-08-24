5d1a9721556716ec635f1c2bcd1c4035acd8d3fc
우디 <woody@kokonut.me>
2023-08-24 16:22:01 +0900

    메인 빌드테스트20

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo su -
 
-# npm의 경로를 변수로 지정
-NPM_PATH=$(which npm)
+# nvm 초기화 및 Node.js 버전 활성화
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # nvm 초기화
 
 PORT=5173
 echo "프로세스 종료용 포트조회 : $PORT"
