f9ebda20b0fb742e5e6a9e4c697699ce34fa01ab
우디 <woody@kokonut.me>
2023-08-24 16:17:48 +0900

    메인 빌드테스트19

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# npm의 경로를 변수로 지정
+# nvm 초기화 및 Node.js 버전 활성화
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # nvm 초기화
+
+# 활성화된 Node.js 버전에 따른 npm의 경로를 변수로 지정
 NPM_PATH=$(which npm)
 
 sudo yum update -y
