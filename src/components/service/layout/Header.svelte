901301b575cd9b6ab076aedda0b3ef20b0f1c84c
조프리 <joffrey@kokonut.me>
2023-09-01 11:38:53 +0900

    로그인 정보 운용 로직의 수정 로그인 정보가 로컬에 남는 문제를 발견해서 해당 사항 개발팀 공조 수정이 필요

--- src/components/service/layout/Header.svelte
+++ src/components/service/layout/Header.svelte
@@ -2,62 +2,16 @@
 	import ServiceHeader from "./ServiceHeader.svelte"
 	import Sider from "./Sider.svelte"
     import {
-        is_login,
         doChangePwdLater,
-        userInfoData, expireDate, mainScreenBlockerVisibility,
+        userInfoData, mainScreenBlockerVisibility,
     } from "../../../lib/store.js"
-    import {beforeUpdate, onMount} from "svelte";
+    import {onMount} from "svelte";
     import CustomConfirm from "../../common/ui/CustomConfirm.svelte";
     import Banner from "../../common/ui/Banner.svelte";
     import MyPagePwd from "../environment/mypage/MyPagePwd.svelte";
-    import {logout} from "../../common/authActions.js";
-    import {ajaxGet, reportCatch} from "../../common/ajax.js";
     import MainScreenBlocker from "../../common/ui/MainScreenBlocker.svelte";
 
-    let isMyPagePwdVisible = false;
-
-    // let auth = false;
-    //
-    // // 화면단 실시간 변경설정
-    beforeUpdate(async () => {
-        is_login.subscribe(value => {
-            // auth = value;
-            if (value === true) {
-                let url = "/v2/api/Admin/authorityCheck"
-
-                ajaxGet(url, false, (res) => {
-                    try {
-                        const userInfo = res.data.sendData;
-                        is_login.set(true);
-                        userInfoData.set(userInfo);
-                        expireDate.set(getFutureDate(Number(userInfo.csAutoLogoutSetting)).toISOString());
-
-                        if (!$doChangePwdLater && userInfo.csPasswordChangeState === '2') {
-                            isMyPagePwdVisible = true;
-                        }
-                    } catch (e) {
-                        reportCatch('temp037', e);
-                    }
-                }, (errCode, errMsg) => {
-                    try {
-                        logout();
-                    } catch (e) {
-                        reportCatch('temp038', e);
-                    }
-                });
-            } else {
-                // alert("세션이 종료되어 로그아웃됩니다.");
-                console.log("로그아웃 하였습니다.");
-                logout();
-            }
-        });
-    });
-
-    function getFutureDate(minutesFromNow) {
-        let futureDate = new Date();
-        futureDate.setMinutes(futureDate.getMinutes() + minutesFromNow);
-        return futureDate;
-    }
+    $: isTimeToChangePassword = !$doChangePwdLater && $userInfoData.csPasswordChangeState === '2';
 
     const reportURLToBeusable = () => {
         const w = window;
@@ -89,4 +43,4 @@
 <MainScreenBlocker  visibility={$mainScreenBlockerVisibility}/>
 
 <!-- 비밀번호 변경 기간 도래에 따른 팝업 -->
-<MyPagePwd bind:visible={isMyPagePwdVisible} regularChangeRoutine={true} />
+<MyPagePwd bind:visible={isTimeToChangePassword} regularChangeRoutine={true} />
