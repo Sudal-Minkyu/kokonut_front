0c5faa4ef2e366c3793284914280715dc39e7ffa
우디 <woody@kokonut.me>
2023-09-01 15:14:44 +0900

    쉘스크립트 수정3

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -3,22 +3,11 @@
 # update packages
 sudo yum update -y
 
-# install node.js
-#curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-#sudo yum install -y nodejs
-
-# install npm packages
-#cd /root/kokonut_frontend/
-#npm install --global npm
-
 # Change directory to the location of package.json file
 cd /root/kokonut_frontend/
 
 # Remove node_modules folder
 rm -rf node_modules
-rm -rf package-lock.json
-
-echo "인스톨시작"
 
 # Install dependencies
 npm install
