02ef391715b559b9f29a3d3d681f94457900b1a8
우디 <woody@kokonut.me>
2023-09-01 14:33:20 +0900

    빌드스펙수정

--- script/start.sh
+++ script/start.sh
@@ -2,10 +2,18 @@
 sudo su -
 cd /root/kokonut_frontend/
 
-PID=$(pgrep -f npm)
-kill $PID
+PORT=5173
+echo "프로세스 종료용 포트조회 : $PORT"
 
-sleep 5
+PID=$(lsof -i :$PORT -t)
+
+if [ -z "$PID" ]; then
+    echo "종료할 프로세스가 없습니다. $PORT"
+else
+    echo "해당 프로세스를 종료합니다. $PORT is: $PID"
+    kill -9 $PID
+    sleep 5
+fi
 
 mkdir /root/kokonut_frontend/logs
 nohup npm run dev -- --host 0.0.0.0 1>/root/kokonut_frontend/logs/$(date +%Y-%m-%d)_stdout.log 2>/root/kokonut_frontend/logs/$(date +%Y-%m-%d)_stderr.log &
