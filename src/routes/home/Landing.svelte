d822c0012860b5cbcdec588e79c74f0607e0bc57
조프리 <joffrey@kokonut.me>
2023-09-04 12:03:39 +0900

    온보딩 신청 양식 틀잡기 (세세한 폰트나 크기 수정 예정)

--- src/routes/home/Landing.svelte
+++ src/routes/home/Landing.svelte
@@ -1,5 +1,7 @@
 <script>
     import { onMount } from "svelte";
+    import 'bootstrap/dist/css/bootstrap.min.css'
+    import ErrorHighlight from "../../components/common/ui/ErrorHighlight.svelte";
 
     let width = window.innerWidth;
 
@@ -56,47 +58,85 @@
         </div>
     </div>
 
-    <div class="inqueryWrite">
-        <div class="inqueryCall">
-            <div class="inqueryGroup">
-                <div>* 선호 온보딩 방식</div>
-                <input type="radio" value="오프라인 미팅" />
-                <input type="radio" value="온라인 교육" />
-                <input type="radio" value="상관없음" />
-            </div>
-            <div class="inqueryGroup">
-                <input type="text" placeholder="이름 *" />
-                <div>* 이름을 입력해주세요.</div>
-            </div>
-            <div class="inqueryGroup">
-                <input type="text" placeholder="회사명 *" />
-                <div>* 회사명을 입력해주세요.</div>
-            </div>
-            <div class="inqueryGroup">
-                <input type="text" placeholder="서비스명 *" />
-                <div>* 서비스명을 입력해주세요.</div>
-            </div>
-            <div class="inqueryGroup">
-                <input type="text" placeholder="연락처 *" />
-                <div>* 연락처를 입력해주세요.</div>
-            </div>
-            <div class="inqueryGroup">
-                <input type="text" placeholder="이메일 *" />
-                <div>* 이메일을 입력해주세요.</div>
-            </div>
-            <div class="inqueryGroup">
-                <textarea placeholder="내용 *"></textarea>
-                <div>[선택]온보딩 진행 시 요청사항</div>
-            </div>
-        </div>
-        <div>
-            <div >
-                <input class="form-check-input" type="checkbox" id="check1">
-                <label class="form-check-input" for="check1"></label>
-                <span style="font-family: 'Cafe24Ohsquareair',serif;font-size: 14px">개인정보 수집 및 이용 동의를 체크해 주세요.<a style="cursor: pointer" onclick="viewTextPopup()">(전문보기)</a></span>
-            </div>
-        </div>
-    </div>
+    <div class="container marT40">
+        <main>
+            <div class="row g-5" style="margin-bottom: 3rem;">
+                <div class="col-md-6 mx-auto">
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <h5 class="form-label">선호 온보딩 방식</h5>
+                            <div class="form-check" style="margin-top: 0.3rem;">
+                                <input id="offlineMeeting" name="preferMethod" type="radio" class="form-check-input">
+                                <label class="form-check-label" for="offlineMeeting">오프라인 미팅</label>
+                            </div>
+                            <div class="form-check">
+                                <input id="onlineMeeting" name="preferMethod" type="radio" class="form-check-input">
+                                <label class="form-check-label" for="onlineMeeting">온라인 교육</label>
+                            </div>
+                            <div class="form-check">
+                                <input id="dontCare" name="preferMethod" type="radio" class="form-check-input">
+                                <label class="form-check-label" for="dontCare">상관없음</label>
+                            </div>
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: -0.5rem 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="custnom" class="form-label">이름</label>
+                            <input type="text" class="form-control" id="custnom">
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: 0 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="companyName" class="form-label">회사명</label>
+                            <input type="text" class="form-control" id="companyName">
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: 0 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="serviceName" class="form-label">서비스명</label>
+                            <input type="text" class="form-control" id="serviceName">
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: 0 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="phoneNumber" class="form-label">연락처</label>
+                            <input type="text" class="form-control" id="phoneNumber" placeholder="010-1234-5678">
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: 0 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="email" class="form-label">이메일</label>
+                            <input type="text" class="form-control" id="email" placeholder="you@example.com">
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: 0 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="requestOnboarding" class="form-label">온보딩 진행시 요청사항 <span class="text-body-secondary">(선택)</span></label>
+                            <textarea class="form-control" id="requestOnboarding" style="resize: none;"></textarea>
+                            <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: 0 0 -0.5rem 0; padding-top: 0.4rem;"/>
+                        </div>
+                    </div>
 
+                    <hr class="my-4">
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="same-address">
+                        <label class="form-check-label" for="same-address">개인정보 수집 및 이용을 동의합니다. (전문보기)</label>
+                    </div>
+                    <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: -0.7rem 0 1rem 0; padding-top: 0.4rem;"/>
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="save-info">
+                        <label class="form-check-label" for="save-info">개인정보 수집 및 이용을 동의합니다. 2</label>
+                    </div>
+                    <ErrorHighlight message="입력해 주세요." fontSize="1.1" style="margin: -0.7rem 0 1rem 0; padding-top: 0.4rem;"/>
+
+                    <button class="w-100 btn btn-light btn-lg" type="submit" style="margin-top: 2rem;">온보딩 신청하기</button>
+                </div>
+            </div>
+        </main>
+    </div>
 </div>
 
+<footer class="my-5 pt-5 text-body-secondary text-center text-small" style="margin-top: 0!important;">
+    <p class="mb-1">&copy; 2017–2023 Company Name</p>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="#">Privacy</a></li>
+        <li class="list-inline-item"><a href="#">Terms</a></li>
+        <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+</footer>
