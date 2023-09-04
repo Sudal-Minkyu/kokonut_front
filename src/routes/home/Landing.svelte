db7ccc5ad8e3347135354353f0c4555873ce2272
조프리 <joffrey@kokonut.me>
2023-09-04 13:36:39 +0900

    부트스트랩 npm 의존성 삭제 후 cdn 링크를 통해 css 만 반영한 버전

--- src/routes/home/Landing.svelte
+++ src/routes/home/Landing.svelte
@@ -1,6 +1,5 @@
 <script>
     import { onMount } from "svelte";
-    import 'bootstrap/dist/css/bootstrap.min.css'
     import ErrorHighlight from "../../components/common/ui/ErrorHighlight.svelte";
 
     let width = window.innerWidth;
@@ -17,7 +16,7 @@
         };
     });
 </script>
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <div class="landing-background"></div>
 {#if width < 768}
     <div class="mobile">
