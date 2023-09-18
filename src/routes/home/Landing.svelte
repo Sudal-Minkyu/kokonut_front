781f5fa9e5ae9e6b3cb79da5e01c3db1a3a82e7e
우디 <woody@kokonut.me>
2023-09-18 22:28:50 +0900

    랜딩페이지 전문보기작업중2

--- src/routes/home/Landing.svelte
+++ src/routes/home/Landing.svelte
@@ -1,6 +1,8 @@
 <script>
     import { onMount } from "svelte";
-    import ErrorHighlight from "../../components/common/ui/ErrorHighlight.svelte";
+    import { fly } from 'svelte/transition';
+    import {findPwd, openDiv, tempPwd} from "../../lib/store.js";
+    import {ajaxBody, ajaxParam, reportCatch} from "../../components/common/ajax.js";
 
     let width = window.innerWidth;
 
@@ -16,8 +18,54 @@
         };
     });
 
+    let popupVisible = false;
+
     function privacyCheck() {
-        console.log("전문보기 클릭!");
+        popupVisible = !popupVisible;
+    }
+
+
+    let iqWriter = undefined; // 작성자
+    let iqState = undefined; // 선호 온보딩 방식 -> 1. 오프라인 미팅 2. 온라인 교육
+    let iqCompany = undefined; // 회사명
+    let iqService = undefined; // 서비스명
+    let iqPhone = undefined; // 연락처(휴대전화)
+    let iqEmail = undefined; // 이메일
+    let iqContents = undefined; // 온보딩 진행 시 요청사항 (이전에 내용칸 활용)
+
+    let essentialCheckMaster = false;
+    function essentialCheck() {
+
+    }
+
+    // 온보딩 신청하기
+    function onbordingSend() {
+
+        // 필수항목 입력했는지 체크하기
+        essentialCheck();
+
+        if(essentialCheckMaster) {
+            let url = "/v1/api/Inquiry/send"
+
+            let sendData = {
+                iqWriter : iqWriter,
+                iqState : iqState,
+                iqCompany : iqCompany,
+                iqService : iqService,
+                iqPhone : iqPhone,
+                iqEmail : iqEmail,
+                iqContents : iqContents
+            }
+
+            ajaxBody(url, sendData, (res) => {
+                try {
+                    $openDiv = 2;
+                    $tempPwd = res.data.sendData.tempPassword;
+                } catch (e) {
+                    reportCatch('temp105', e);
+                }
+            });
+        }
     }
 
 </script>
@@ -37,35 +85,37 @@
 
     <div class="onbording_container">
 
-        <div class="view_text_popup">
-            <div class="inquiry_box__close"></div>
-            <div class="view_text_popup_contents">
-                <p>이용자가 입력하는 문의사항을 처리하기 위해</p>
-                <p style="margin-bottom: 20px;">다음과 같이 개인정보를 수집합니다.</p>
-                <table>
-                    <tbody class="viewTextTable">
-                    <tr>
-                        <td style="width: 140px">처리 목적</td>
-                        <td style="width: 200px">수집 항목</td>
-                        <td style="width: 150px">수집 방법</td>
-                        <td style="width: 180px">처리 및 보유 기간</td>
-                    </tr>
-                    <tr>
-                        <td>문의사항 처리</td>
-                        <td>이름, 회사명, 서비스명, 연락처, 이메일</td>
-                        <td>이용자의 입력</td>
-                        <td>1년</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <p style="margin-top: 20px;">이용자는 위와 같이 개인정보를 처리하는데</p>
-                <p style="margin-bottom: 10px;">동의하지 않으실 수 있습니다.</p>
-                <p>다만, 동의하지 않으시면 문의</p>
-                <p>서비스를 받으실 수 없습니다.</p>
-            </div>
-        </div>
-
         <div class="onbording_write">
+
+            {#if popupVisible}
+                <div class="view_text_popup" in:fly={{ y: -200 }} out:fly={{ y: -200 }}>
+                    <div class="view_text_popup_contents">
+                        <div class="inquiry_box__close" on:click={privacyCheck}></div>
+                        <p>이용자가 입력하는 온보딩 신청을 처리하기 위해</p>
+                        <p style="margin-bottom: 20px;">다음과 같이 개인정보를 수집합니다.</p>
+                        <table>
+                            <tbody class="viewTextTable">
+                            <tr>
+                                <td style="width: 140px">처리 목적</td>
+                                <td style="width: 200px">수집 항목</td>
+                                <td style="width: 150px">수집 방법</td>
+                                <td style="width: 180px">처리 및 보유 기간</td>
+                            </tr>
+                            <tr>
+                                <td>온보딩 신청처리</td>
+                                <td>이름, 회사명, 서비스명,<br/>연락처, 이메일</td>
+                                <td>이용자의 입력</td>
+                                <td>1년</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <p style="margin-top: 20px;">이용자는 위와 같이 개인정보를 처리하는데</p>
+                        <p style="margin-bottom: 10px;">동의하지 않으실 수 있습니다.</p>
+                        <p>다만, 동의하지 않으시면 온보딩교육</p>
+                        <p>서비스를 받으실 수 없습니다.</p>
+                    </div>
+                </div>
+            {/if}
 
             <div >
                 <div class="onbording_deps">
@@ -73,32 +123,31 @@
                     <div class="onbording_select">
                         <input type="radio" id="select" name="iqState"><label for="select">오프라인 미팅</label>
                         <input type="radio" id="select2" name="iqState"><label for="select2">온라인 교육</label>
-                        <input type="radio" id="select3" name="iqState"><label for="select3">상관없음</label>
                     </div>
                 </div>
                 <div class="onbording_deps">
                     <label for="userName" class="onbording_label"><a>*</a> 이름 <p class="onbording_checkmark">이름을 입력해 주세요.</p></label>
-                    <input type="text" class="onbording_input" id="userName" placeholder="이름을 입력해 주세요.">
+                    <input type="text" class="onbording_input" id="userName" bind:value={iqWriter} placeholder="이름을 입력해 주세요.">
                 </div>
                 <div class="onbording_deps">
                     <label for="companyName" class="onbording_label"><a>*</a> 회사명 <p class="onbording_checkmark">회사명을 입력해 주세요.</p></label>
-                    <input type="text" class="onbording_input" id="companyName" placeholder="회사명을 입력해 주세요.">
+                    <input type="text" class="onbording_input" id="companyName" bind:value={iqCompany} placeholder="회사명을 입력해 주세요.">
                 </div>
                 <div class="onbording_deps">
                     <label for="serviceName" class="onbording_label"><a>*</a> 서비스명 <p class="onbording_checkmark">서비스명을 입력해 주세요.</p></label>
-                    <input type="text" class="onbording_input" id="serviceName" placeholder="서비스명을 입력해 주세요.">
+                    <input type="text" class="onbording_input" id="serviceName" bind:value={iqService} placeholder="서비스명을 입력해 주세요.">
                 </div>
                 <div class="onbording_deps">
                     <label for="phoneNumber" class="onbording_label"><a>*</a> 연락처 <p class="onbording_checkmark">연락처를 입력해 주세요.</p></label>
-                    <input type="text" class="onbording_input" id="phoneNumber" placeholder="연락처를 입력해 주세요.">
+                    <input type="text" class="onbording_input" id="phoneNumber" bind:value={iqPhone} placeholder="연락처를 입력해 주세요.">
                 </div>
                 <div class="onbording_deps">
                     <label for="userEmail" class="onbording_label"><a>*</a> 이메일 <p class="onbording_checkmark">이메일을 입력해 주세요.</p></label>
-                    <input type="text" class="onbording_input" id="userEmail" placeholder="이메일을 입력해 주세요.">
+                    <input type="text" class="onbording_input" id="userEmail" bind:value={iqEmail} placeholder="이메일을 입력해 주세요.">
                 </div>
                 <div class="onbording_deps">
                     <label for="requestOnboarding" class="onbording_label">온보딩 진행시 요청사항 <span class="text-body-secondary">(선택)</span></label>
-                    <textarea class="onbording_textarea" id="requestOnboarding"></textarea>
+                    <textarea class="onbording_textarea" id="requestOnboarding" bind:value={iqContents}></textarea>
                 </div>
             </div>
 
@@ -110,7 +159,7 @@
                 <p class="onbording_privacy_check">개인정보 수집 및 이용을 동의해 주세요.</p>
             </div>
 
-            <button class="onbording_btn" type="button">온보딩 신청하기</button>
+            <button class="onbording_btn" type="button" on:click={onbordingSend}>온보딩 신청하기</button>
 
             <p class="onbording_footer">
                 Copyright© 2023 Everyfeb<br/>All Rights Reserved
