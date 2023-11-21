19ac4e0f7a5030b0f5664c7c283413f678a872da
우디 <woody@kokonut.me>
2023-11-21 11:22:28 +0900

    buildspec.yml 수정본3

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # update packages
-#sudo yum update -y
+sudo yum update -y
 
 # install node.js
 #curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
@@ -13,10 +13,10 @@
 #npm install --global npm
 
 # Change directory to the location of package.json file
-#cd /root/kokonut_frontend/
+cd /root/kokonut_frontend/
 
 # Remove node_modules folder
-#rm -rf node_modules
+rm -rf node_modules
 
 # Install dependencies
-#npm install
+npm install
